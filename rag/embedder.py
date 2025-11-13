a412c04486acbd6e5dd411fa87302d9c1053e255
Sarthak <2sarthaksharma@gmail.com>
2025-11-14 01:54:26 +0530

    Added rag folder and created class to call embedding model

---
+++ rag/embedder.py
@@ -0,0 +1,15 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from core.config import get_settings
+
+class Embedder:
+    """
+    Loads the embedding model used for the RAG pipeline.
+    """
+    settings = get_settings()
+    @staticmethod 
+    def get_embedder() :
+        return GoogleGenerativeAIEmbeddings(
+            model = "gemini-embedding-001" , 
+            api_key = Embedder.settings.GOOGLE_API_KEY
+    )
+        
