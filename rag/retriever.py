d8f993a6237233eac3fec4216c60f1dcdcf428f4
Sarthak <2sarthaksharma@gmail.com>
2025-11-14 02:13:17 +0530

    created the retriver that will be used in rag chain

---
+++ rag/retriever.py
@@ -0,0 +1,17 @@
+class Retriever :
+    """
+    Creates a retriever from a vector store.
+    This retriever will be used by the QA pipeline to fetch
+    relevant document chunks for answering user queries.
+    """
+    @staticmethod
+
+    def build_retriever(vectorstore , k : int = 3) :
+        """
+        Convert vectorstore into a retriever.
+        """
+        retriver = vectorstore.as_retriever(search_type = "similarity" , k = k)
+        return retriver
+
+
+         
